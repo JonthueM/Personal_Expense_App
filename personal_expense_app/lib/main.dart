2d90a1ba7ada7f1658490b16d83b9ceb9ea35ddb
JonthueM <admin@jonthuemichel.com>
2022-03-20 20:48:47 -0400

    Update

--- personal_expense_app/lib/main.dart
+++ personal_expense_app/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,16 +15,23 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1',
+        title: 'New Shoes',
+        amount: 69.99,
+        date: DateTime
+            .now()), //From transaaction class within this < > which you instiante
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 16.53,
+        date: DateTime.now())
+  ];
 
-  final List<Transaction> transactions =[
-  Transaction(
-    id: 't1', 
-    title: 'New Shoes', 
-    amount:69.99, 
-    date: DateTime.now()
-    ), //From transaaction class within this < > which you instiante
-  Transaction(id: 't2', title: 'Weekly Groceries', amount: 16.53, date: DateTime.now())
-];
+  String titleInput;
+  //String amountInput;
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,59 +40,94 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start, //Center within Colum
-        crossAxisAlignment: CrossAxisAlignment.start,
+        // mainAxisAlignment: MainAxisAlignment.start, //Center within Colum
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Card(child: Container(
-
-            width: double.infinity,
-            color: Colors.red,
-            child: Text('CHART')),
-          elevation: 5,
+          Card(
+            child: Container(
+                width: double.infinity,
+                color: Colors.red,
+                child: Text('CHART')),
+            elevation: 5,
           ),
-          Column(children: transactions.map((tx){
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    onChanged: (value) {
+                      titleInput = value;
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    //onChanged: (value) => amountInput = value,
+                    controller: amountController,
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text('Add Transaction'),
+                    textColor: Colors.purple,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
               return Card(
-                child:Row(
+                child: Row(
                   children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                    decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2,),), 
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      tx.amount.toString(),
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,  
-                        
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
                       ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${tx.amount}', // String interpolation
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
                       ),
-                      ),
-                      Column(  
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
                         Text(
-                          tx.title, 
-                      textAlign: TextAlign.left,
-                          style:TextStyle(
+                          tx.title,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 15,
                           ),
-
-                          ),
+                        ),
                         Text(
-                          tx.date.toString(), 
+                          DateFormat('yyyy-MM-dd').format(tx.date),
                           textAlign: TextAlign.left,
                           style: TextStyle(
                             color: Colors.blueGrey,
                             fontSize: 12,
                           ),
-                          ),
-                      ],)
-                ],),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               );
-          }).toList(),)
+            }).toList(),
+          )
         ],
       ),
     );
